4b4fb05c15b74422b946733f42203b3d8c45264a
Sohaila-Mohsen <73060748+Sohaila-Mohsen@users.noreply.github.com>
2023-06-28 19:32:20 +0300

    forgrt password

--- lib/views/common/create_new_pass_screen.dart
+++ lib/views/common/create_new_pass_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vewww/bloc/auth_cubit/auth_cubit.dart';
 
 import '../../core/components/backward_arrow.dart';
 import '../../core/components/custom_text_field.dart';
@@ -6,8 +8,10 @@ import '../../core/components/default_button.dart';
 import '../../core/components/pop_up.dart';
 
 class CreateNewPassScreen extends StatelessWidget {
-   final GlobalKey<FormState> _formKey = GlobalKey();
+  CreateNewPassScreen({required this.email, Key? key}) : super(key: key);
+  final GlobalKey<FormState> _formKey = GlobalKey();
   final TextEditingController _newPassword = TextEditingController();
+  String email;
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +23,7 @@ class CreateNewPassScreen extends StatelessWidget {
         leading: BackwardArrow(function: () {
           Navigator.pop(context);
         }),
-        title: Center(
+        title: const Center(
           child: Text(
             'Change Password',
             style: TextStyle(
@@ -42,26 +46,25 @@ class CreateNewPassScreen extends StatelessWidget {
                   height: 250,
                   width: 250,
                 ),
-                
-                SizedBox(
+                const SizedBox(
                   height: 15,
                 ),
                 CustomTextField(
                   controller: _newPassword,
                   validator: (value) {
-                    RegExp regex = RegExp(
-                        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~])');
+                    RegExp regex =
+                        RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])');
                     if (value == null || value.isEmpty) {
                       return 'Password is required';
                     }
                     if (!regex.hasMatch(value)) {
-                      return 'Password should Contain: \nAt least 8 chatacters\nAt least an uppercase latter  \nAt least a lowercase latter   \nAt least one number \nAt least one special character';
+                      return 'Password should Contain: \nAt least 8 chatacters\nAt least an uppercase latter  \nAt least a lowercase latter   \nAt least one number';
                     }
                     return null;
                   },
                   label: 'New Password',
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 15,
                 ),
                 CustomTextField(
@@ -75,20 +78,38 @@ class CreateNewPassScreen extends StatelessWidget {
                   },
                   label: 'Confirm Password',
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
-                defaultButton(
-                    text: 'Continue',
-                    function: () {
-                      if (_formKey.currentState!.validate()) {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) =>
-                              popUp(context, titleOFPopUp: 'New password created Successfully'),
-                        );
-                      }
-                    }),
+                BlocBuilder<AuthCubit, AuthState>(
+                  builder: (context, state) {
+                    return defaultButton(
+                        text: (state is ResetPasswordLoadingState)
+                            ? "Loading..."
+                            : 'Continue',
+                        function: () async {
+                          AuthCubit authCubit = AuthCubit.get(context);
+                          if (_formKey.currentState!.validate()) {
+                            await authCubit.resetPassword(
+                                email, _newPassword.text);
+                            if (state is ResetPasswordSuccessState) {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) => popUp(
+                                    context,
+                                    titleOFPopUp:
+                                        'New password created Successfully'),
+                              );
+                            } else if (state is ResetPasswordErrorState) {
+                              var snackBar =
+                                  SnackBar(content: Text(state.errMessage));
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                            }
+                          }
+                        });
+                  },
+                )
               ],
             ),
           ),
---
+++ lib/views/common/forgot_password_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vewww/bloc/auth_cubit/auth_cubit.dart';
+import 'package:vewww/core/components/backward_arrow.dart';
+import 'package:vewww/core/components/custom_text_field.dart';
+import 'package:vewww/core/components/default_button.dart';
+import 'package:vewww/core/components/forgot_pass_car.dart';
+import 'package:vewww/core/style/app_Text_Style/app_text_style.dart';
+import 'package:vewww/views/common/create_new_pass_screen.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  ForgotPasswordScreen({Key? key}) : super(key: key);
+  TextEditingController _email = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: BackwardArrow(function: () {
+          Navigator.pop(context);
+        }),
+        title: const Center(
+          child: Text(
+            'Forgot Password',
+            style: TextStyle(
+              color: Color.fromRGBO(2, 113, 106, 1),
+              fontSize: 23,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/password.png',
+                height: 250,
+                width: 250,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Please enter your email:",
+                style: AppTextStyle.greyStyle(),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomTextField(
+                validator: (v) {},
+                label: "Email",
+                controller: _email,
+              ),
+              Container(height: 100),
+              BlocBuilder<AuthCubit, AuthState>(
+                builder: (context, state) {
+                  return defaultButton(
+                      text: (state is SendingCodeLoadingState)
+                          ? "Loading..."
+                          : 'Continue',
+                      function: () async {
+                        if (_email.text.isNotEmpty) {
+                          AuthCubit authCubit = AuthCubit.get(context);
+                          await authCubit.sendResetPasswordCode(_email.text);
+                          if (state is SendingCodeSuccessState) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => CreateNewPassScreen(
+                                        email: _email.text,
+                                      )),
+                            );
+                          } else if (state is SendingCodeErrorState) {
+                            var snackBar =
+                                SnackBar(content: Text(state.errMessage));
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          }
+                        } else {
+                          var snackBar =
+                              SnackBar(content: Text("Email is Required"));
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        }
+                      });
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
